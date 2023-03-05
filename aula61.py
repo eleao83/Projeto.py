f3ad9234c73634c89171f6fe03320a702ce2cc6b
eleao83 <andriy.dudu@icloud.com>
2023-03-05 20:41:33 -0300

    update

--- aula61.py
+++ aula61.py
@@ -23,3 +23,16 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
+
+cpf = '74682489070'
+nove_digitos = cpf[:9] #Fatiamento 
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1 
+    contador_regressivo_1 -= 1
+digito_1 =    (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
+
