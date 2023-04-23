9d204306c0947bebec4f3e5ca3c6a264b6a9f0c4
eleao83 <andriy.dudu@icloud.com>
2023-04-23 15:26:20 -0300

    update

--- aula90.py
+++ aula90.py
@@ -1,7 +1,18 @@
 # Generator expression, Iterables e Iterators em Python 
+
+import sys
 iterable = ['Eu', 'Tenho', '_iter_']
 iterator = iter(iterable) # tem _inter_ e _next_
 
+"""
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
+"""
+
+# aula90 2
+lista= [n for n in range (1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
