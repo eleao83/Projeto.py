3f0b65e57cb7d6e57b298669e26d58150719f1ec
eleao83 <andriy.dudu@icloud.com>
2023-07-15 15:32:38 -0300

    update

---
+++ Aula141/log.py
@@ -0,0 +1,38 @@
+# Abstração
+# Herança - é um 
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o métado log')
+    
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_sucesso(self, msg):
+        return self._log(f'Sucesso: {msg}')
+
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log:', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')        
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_sucesso('Que lega')
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_sucesso('Que legal')
